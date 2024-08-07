ba940763a9c53d88aeb8072d2a4482c85c0f7aee
ruxeyy04 <ruxepasok356@gmail.com>
2024-08-07 16:43:49 +0800

    update

--- app/(auth)/signin.tsx
+++ app/(auth)/signin.tsx
@@ -62,7 +62,7 @@ const SignIn = () => {
         <View
           className="w-full flex justify-center h-full px-4 my-6"
           style={{
-            minHeight: Dimensions.get("window").height - 100,
+            minHeight: Dimensions.get("window").height - 200,
           }}
         >
           <Image
