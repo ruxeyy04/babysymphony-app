306a30b0471fb98c0e292d40bd7401c61dc125d1
ruxeyy04 <ruxepasok356@gmail.com>
2024-08-11 13:04:31 +0800

    fix: update

--- constants/images.ts
+++ constants/images.ts
@@ -1,4 +1,4 @@
 import logo from '../assets/images/lullaby-logo.png'
-import logowhite from '../assets/images/lullaby-logo -white.png'
+import logowhite from '../assets/images/lullaby-logo-white.png'
 
 export default { logo, logowhite }
