84892a26a4c42dc37fb43a12bf4e42dcdbc8c323
John Micky Butnande <90739127+jmikeyey@users.noreply.github.com>
2024-08-15 22:17:26 +0800

    feat: new utility function for fetching google profile

---
+++ utils/fetchGoogleUserProfile.tsx
@@ -0,0 +1,21 @@
+export const fetchGoogleUserProfile = async (accessToken: string) => {
+  try {
+    const response = await fetch(
+      "https://www.googleapis.com/oauth2/v3/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch user profile");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching user profile:", error);
+    return null;
+  }
+};
