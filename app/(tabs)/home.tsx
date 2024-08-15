b4e6fa3794395c29feca049f9f0324864e6cd75d
John Micky Butnande <90739127+jmikeyey@users.noreply.github.com>
2024-08-15 22:19:06 +0800

    new: display data on home on signedin account

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,16 +1,20 @@
-import { View, Text, Image } from 'react-native';
-import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { StatusBar } from 'expo-status-bar';
-import { Images } from '@/constants';
-import { ThemeProvider, useTheme } from '@/context/ThemeContext';
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemeProvider, useTheme } from "@/context/ThemeContext";
+import { useGoogleAuth } from "@/hooks/useGoogleAuth";
 
 const Home = () => {
-  const {currentTheme} = useTheme()
+  const { currentTheme } = useTheme();
+  const { authData } = useGoogleAuth();
   return (
     <ThemeProvider>
-      <SafeAreaView className="flex-1 " style={{backgroundColor: currentTheme.background}}>
-        <View className='items-center justify-center flex-1'>
+      <SafeAreaView
+        className="flex-1 "
+        style={{ backgroundColor: currentTheme.background }}
+      >
+        <Text>{JSON.stringify(authData)}</Text>
+        <View className="items-center justify-center flex-1">
           <Image
             source={currentTheme.applogo}
             className="w-[300px] h-[78px]"
@@ -18,7 +22,12 @@ const Home = () => {
           />
         </View>
         <View className="flex-1 justify-center items-center">
-          <Text className="text-2xl font-bold " style={{color: currentTheme.textColor}}>Welcome to Home</Text>
+          <Text
+            className="text-2xl font-bold "
+            style={{ color: currentTheme.textColor }}
+          >
+            Welcome to Home
+          </Text>
         </View>
       </SafeAreaView>
     </ThemeProvider>
